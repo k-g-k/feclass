56a872109cd95e423b696109baa121869bb717e2
Grace Kohut <gk0126@gmail.com>
2015-03-09 12:36:15 -0400

    whole bunch of mess

--- app.js
+++ app.js
@@ -12,10 +12,19 @@ var express = require('express'),
 //Allow JSON posts/puts
 app.use(bodyParser.json());
 
+// Specify src as the place where public files are found
+app.use(express.static(__dirname + '/src'));
+
 app.get('/api/friends', function (req, res) {
   // res.send('Hello, world');
   res.json(friends);
 });
+
+app.post('/api/friends', function (req, res) {
+  friends.push(req.body);
+  res.json(req.body);
+});
+
 
 // Start the server
 var server = app.listen(process.env.PORT || 3000, function () {
