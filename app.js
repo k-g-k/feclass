1b57bf2bff658d853d1979d2019db8b29901a157
Grace Kohut <gk0126@gmail.com>
2015-03-09 11:42:53 -0400

    added json

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     app = express();
+    friends = [{
+      name: 'Matt',
+      gender: 'm'
+      }, {
+      name: 'Sally',
+      gender: 'f'
+    }];
+
+//Allow JSON posts/puts
+app.use(bodyParser.json());
 
 app.get('/api/friends', function (req, res) {
-  res.send('Hello, world');
+  // res.send('Hello, world');
+  res.json(friends);
 });
 
 // Start the server
